787d9fabc02f71f9e69dc4f44aa059ca48ca5399
MikKut <69770483+MikKut@users.noreply.github.com>
2021-05-12 09:42:41 +0300

    Add files via upload

---
+++ CSV/ConsoleApp9/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Task2
+{
+    public class Person
+    {
+        public Person(string fn, string secn, string surn)
+        {
+            this.FirstName = fn;
+            this.SecondName = secn;
+            this.Surname = surn;
+        }
+
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Surname {  get; set; }
+
+        public string GetTheProperty(string name)
+        {
+
+            return this.GetType().GetProperty(name).GetValue(this, null).ToString();
+        }
+
+    }
+}
