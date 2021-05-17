5e64d7266eb2a4ac9fb6c729d201be907721c475
MikKut <69770483+MikKut@users.noreply.github.com>
2021-05-17 10:30:32 +0300

    Add files via upload

---
+++ CSV/ConsoleApp9/PersonsDataHendler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace Task2
+{
+    public class PersonsDataHendler
+    {
+       public static string[] ParseToPersonsFields()
+        {
+             string str = Console.ReadLine();
+
+            string[] namesArr = str.Split(",");
+
+            return namesArr;
+
+        }     
+        
+    }
+
+}
