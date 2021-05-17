882f1141f66299216710a9914f2484deb8a6fe3b
MikKut <69770483+MikKut@users.noreply.github.com>
2021-05-17 10:18:01 +0300

    Add files via upload

---
+++ BullsAndCowsFinal/BullsAndCows/BullsAndCows/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BullsAndCows
+{
+    public class GameManager
+    {
+        private Game game;
+
+        public GameManager(Game game)
+        {
+            this.game = game;
+        }
+
+        public void Start()
+        {
+            var gameContinues = game.MakeMove();
+
+            var counter = 0;
+
+            while (gameContinues)
+            {
+                counter++;
+
+                gameContinues = game.MakeMove();
+            }
+        }
+    }
+}
