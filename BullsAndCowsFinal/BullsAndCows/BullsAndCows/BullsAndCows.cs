966f57b6ae0341bf28792beb0e8947e372dca1e4
MikKut <69770483+MikKut@users.noreply.github.com>
2021-05-17 09:50:48 +0300

    Add files via upload

---
+++ BullsAndCowsFinal/BullsAndCows/BullsAndCows/BullsAndCows.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullsAndCows
+{
+    public class BullsAndCows
+    { 
+        public int Bulls { get; set; }
+
+        public int Cows { get; set; }
+
+        public BullsAndCows(int bulls, int cows)
+        {
+            this.Bulls = bulls;
+            this.Cows = cows;     
+        }
+        public bool Equals(BullsAndCows bullsAndCows)
+        {
+            if (this.Bulls == bullsAndCows.Bulls && this.Cows == bullsAndCows.Cows)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
